fc112f07b46d9731448666509656d959d45bc170
Pijus Bilinskas <pijus.bilins@gmail.com>
2025-06-26 15:15:35 +0300

    fixed?????

--- app/category/[id]/page.tsx
+++ app/category/[id]/page.tsx
@@ -4,9 +4,17 @@ import { getBooksByCategory } from "@/lib/books"
 
 
 
-export default async function CategoryPage ({params}: {params: {id: string}}) {
+export default async function CategoryPage ({params}: {params: Promise<{id: string}>}) {
         const { id } = await params;
         const books = await getBooksByCategory({ category: id })
+
+         if (!books) {
+    return (
+      <div>
+        <h1>Book not found</h1>
+      </div>
+    );
+     }
 
     return(
         <div>
