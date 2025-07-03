5e5ddb7b731bd71aad084715d4e3a44a5c20969c
Pijus Bilinskas <pijus.bilins@gmail.com>
2025-07-03 15:31:14 +0300

    fixed type error

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,8 +2,8 @@ import { Client, Databases, Storage, Account } from "appwrite";
 
 const client = new Client();
 client
-    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!)
+    .setEndpoint(`${process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT}`)
+    .setProject(`${process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID}`)
 
 const databases = new Databases(client);
 const storage = new Storage(client);
